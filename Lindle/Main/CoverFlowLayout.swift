ed70794b59ac5eb362a203460fbee977a193a0de
lilikazine <lilikazine@gmail.com>
2018-11-10 22:02:52 +0800

    unfinished

---
+++ Lindle/Main/CoverFlowLayout.swift
@@ -0,0 +1,19 @@
+//
+//  CoverFlowLayout.swift
+//  Lindle
+//
+//  Created by LiLi Kazine on 2018/11/10.
+//  Copyright © 2018 HNA Group Co.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class CoverFlowLayout: UICollectionViewLayout {
+
+    var cellAttrsDictionary = Dictionary<IndexPath, UICollectionViewLayoutAttributes>()
+
+    var dataSourceDidUpdate = true
+    
+    
+    
+}
